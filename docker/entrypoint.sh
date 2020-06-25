7b296e7338fad1f9d8935da910f6fa09b26b59d9
Alexander Ivanovsky <1336594+SUNx2YCH@users.noreply.github.com>
2020-06-25 16:40:50 +0300

    Fix cleanup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -eu
 
+# cleanup
+trap "rm -frv dist/ *.egg-info/" EXIT
+
 cd $INPUT_PROJECT_DIR
 python setup.py $INPUT_SETUP_COMMAND
 twine upload dist/*
-rm -frv dist/ *.egg-info/
